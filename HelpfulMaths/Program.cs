8a0177a7d93602e81bf309a26f7e8c65f928c19f
James Absalem Lintu <26407837+jamesabsalem@users.noreply.github.com>
2019-05-21 20:13:50 +0600

    first commit

---
+++ HelpfulMaths/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace HelpfulMaths
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            var numbersList = input.Split('+').Select(x => int.Parse(x)).ToList();
+            var sortednumber = numbersList.OrderBy(x => x);
+
+            var stringBuilder = new StringBuilder();
+            foreach (var i in sortednumber)
+            {
+                stringBuilder.Append(i);
+                stringBuilder.Append("+");
+            }
+
+            stringBuilder.Remove(stringBuilder.Length - 1, 1);
+            Console.WriteLine(stringBuilder);
+        }
+    }
+}
